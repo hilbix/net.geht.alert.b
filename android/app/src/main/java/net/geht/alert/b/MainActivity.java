927812675ed03d57495a117ca1ec86a520a30c17
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-03-15 23:40:31 +0100

    android/app/src/main/java/net/geht/alert/b/MainActivity.java modified

--- android/app/src/main/java/net/geht/alert/b/MainActivity.java
+++ android/app/src/main/java/net/geht/alert/b/MainActivity.java
@@ -6,8 +6,11 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+// See https://github.com/flutter/flutter/issues/47153#issuecomment-596145324
+// and https://github.com/flutter/website/blob/3928650601ac66c40db77b3eb2f28c54bebe8a0d/src/docs/development/packages-and-plugins/plugin-api-migration.md (upgrade part 4 and 5)
+// with https://github.com/FirebaseExtended/flutterfire/blob/4d7b2bdd7fbdee65551754968b140653ea16128f/packages/firebase_messaging/pubspec.yaml
+//  @Override
+//  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//    GeneratedPluginRegistrant.registerWith(flutterEngine);
+//  }
 }
